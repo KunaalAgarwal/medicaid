b4dddf660b8a1da1610cb86e7a8e4f059352d404
KunaalAgarwal <aad5ha@virginia.edu>
2023-06-05 15:09:02 -0400

    added filtering methods

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-
-function main(){
-    return runApi();
+Medicaid = (await fetch("https://data.medicaid.gov/api/1/metastore/schemas/dataset/items")).json();
+data = (await fetch(`https://data.medicaid.gov/api/1/datastore/query/${Medicaid.find(obj => obj.title === wiki)["identifier"]}/0`)).json()
+console.log(data);
+async function logJSONData() {
+    const response = await fetch("http://example.com/movies.json");
+    return await response.json();
 }
 
-function runApi(){
-    //implement
+
+async function logMedicaidJSONData() {
+    const response = await fetch("https://data.medicaid.gov/api/1/metastore/schemas/dataset/items");
+    // console.log(response);
+    return await response.json();
 }
+
+
+
