1d3209bbbe213338d6c3fb030d86ff8e2e23c6f2
KunaalAgarwal <aad5ha@virginia.edu>
2023-06-06 15:02:04 -0400

    expanded fetchItems() method to work on all endpoints

---
+++ metastore.js
@@ -0,0 +1,64 @@
+// title: '2017 Child and Adult Health Care Quality Measures'
+// let s = `Performance rates on frequently reported health care quality measures in the CMS Medicaid/CHIP Child and Adult Core Sets, for FFY 2017 reporting.  Source: Mathematica analysis of MACPro and Form CMS-416 reports for the FFY 2017 reporting cycle. For more information, see the <a href="https://www.medicaid.gov/medicaid/quality-of-care/performance-measurement/child-core-set/index.html">Children's Health Care Quality Measures</a> and <a href="https://www.medicaid.gov/medicaid/quality-of-care/performance-measurement/adult-core-set/index.html">Adult Health Care Quality Measures</a> webpages.`
+// keyword 'performance rates'
+// identifier: 'c1028fdf-2e43-5d5e-990b-51ed03428625'
+import {fetchItems} from './sdk.js';
+async function getItemByTitleName(databaseTitle) {
+    try{
+        const items =  await fetchItems("metastore/schemas/dataset/items");
+        return items.filter(item => item.title.toLocaleUpperCase() === databaseTitle.toLocaleUpperCase());
+    } catch (Error){
+        console.log("The request could not be fulfilled.");
+    }
+
+}
+
+async function getItemByKeyword(dbKeyword){
+    try{
+        const items =  await fetchItems("metastore/schemas/dataset/items");
+        return items.filter(item => item.keyword.some(key => key.toLocaleUpperCase() === dbKeyword.toLocaleUpperCase()));
+    }catch (Error){
+        console.log("The request could not be fulfilled.");
+    }
+}
+
+async function getItemByDescription(dbDescription) {
+    try{
+        const items =  await fetchItems("metastore/schemas/dataset/items");
+        return items.filter(item => item.description.toLocaleUpperCase() === dbDescription.toLocaleUpperCase());
+    }catch (Error){
+        console.log("The request could not be fulfilled.");
+    }
+}
+
+async function getItemByIdentifier(dbIdentifier){
+    try {
+        return await fetchItems("metastore/schemas/dataset/items/" + dbIdentifier);
+    } catch (Error){
+        console.log("The request could not be fulfilled.");
+    }
+}
+
+async function filterItemsByIdentifier(dbIdentifier) {
+    try{
+        const items =  await fetchItems("metastore/schemas/dataset/items");
+        return items.filter(item => item.identifier.toLocaleUpperCase() === dbIdentifier.toLocaleUpperCase());
+    }catch (Error){
+        console.log("The request could not be fulfilled.");
+    }
+}
+
+
+export {
+    getItemByTitleName,
+    getItemByKeyword,
+    getItemByDescription,
+    getItemByIdentifier,
+    filterItemsByIdentifier
+}
+
+getItemByIdentifier('c1028fdf-2e43-5d5e-990b-51ed03428625').then(r => console.log(r));
+getItemByIdentifier('c1028fdf-2e43-5d5e-990b-51ed03428625').then(r => console.log(r));
+
+filterItemsByIdentifier('c1028fdf-2e43-5d5e-990b-51ed03428625').then(r => console.log(r));
+getItemByTitleName('2017 Child and Adult Health Care Quality Measures').then(r => console.log(r));
--- sdk.js
+++ sdk.js
@@ -1,55 +1,34 @@
-// title: '2017 Child and Adult Health Care Quality Measures'
-// let s = `Performance rates on frequently reported health care quality measures in the CMS Medicaid/CHIP Child and Adult Core Sets, for FFY 2017 reporting.  Source: Mathematica analysis of MACPro and Form CMS-416 reports for the FFY 2017 reporting cycle. For more information, see the <a href="https://www.medicaid.gov/medicaid/quality-of-care/performance-measurement/child-core-set/index.html">Children's Health Care Quality Measures</a> and <a href="https://www.medicaid.gov/medicaid/quality-of-care/performance-measurement/adult-core-set/index.html">Adult Health Care Quality Measures</a> webpages.`
-// keyword 'performance rates'
-// identifier: 'c1028fdf-2e43-5d5e-990b-51ed03428625'
-
 const loadedAt = `Medicaid SDK loaded at \n${Date()}`;
-let itemsPromise = null; // caching
+const baseUrl = "https://data.medicaid.gov/api/1/";
+let cache = {}
 
-async function fetchItems() {
-    if (itemsPromise !== null) {
-        return itemsPromise;
+
+async function fetchItems(endpoint) {
+    if (cache[endpoint] !== undefined) {
+        return cache[endpoint];
     } else {
-        itemsPromise = fetch('https://data.medicaid.gov/api/1/metastore/schemas/dataset/items')
-            .then((response) => response.json())
+        cache[endpoint] = fetch(baseUrl + endpoint)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('API response was invalid');
+                }
+                return response.json();
+            })
+            .then((data) => {
+                cache[endpoint] = data; // Cache the response
+                return data;
+            })
             .catch((error) => {
-                itemsPromise = null; // Reset the promise if an error occurs
+                delete cache[endpoint]; // Remove the entry from cache in case of error
                 throw error;
             });
-
         console.log(loadedAt);
-        return itemsPromise;
+        return cache[endpoint];
     }
 }
 
-async function getItemByTitleName(databaseTitle) {
-    const items =  await fetchItems();
-    return items.filter(item => item.title.toLocaleUpperCase() === databaseTitle.toLocaleUpperCase());
-}
-
-async function getItemByKeyword(dbKeyword){
-    const items = await fetchItems();
-    return items.filter(item => item.keyword.some(key => key.toLocaleUpperCase() === dbKeyword.toLocaleUpperCase()));
-}
-
-async function getItemByDescription(dbDescription) {
-    const items = await fetchItems();
-    return items.filter(item => item.description.toLocaleUpperCase() === dbDescription.toLocaleUpperCase());
-}
-
-async function getItemByIdentifier(dbIdentifier) {
-    const items = await fetchItems();
-    return items.filter(item => item.identifier.toLocaleUpperCase() === dbIdentifier.toLocaleUpperCase());
-}
 
 export {
-    fetchItems,
-    getItemByTitleName,
-    getItemByKeyword,
-    getItemByDescription,
-    getItemByIdentifier
+    fetchItems
 }
-
-// getItemByIdentifier('c1028fdf-2e43-5d5e-990b-51ed03428625').then(r => console.log(r));
-// getItemByIdentifier('c1028fdf-2e43-5d5e-990b-51ed03428625').then(r => console.log(r));
 
