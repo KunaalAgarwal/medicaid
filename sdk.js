7634073f2ddff5f56498f4a976a567e465560b65
KunaalAgarwal <aad5ha@virginia.edu>
2023-06-06 10:50:31 -0400

    added links to code and issues by adjusting index.html and styles.css

--- sdk.js
+++ sdk.js
@@ -52,6 +52,7 @@ class MedicaidSDK {
 //     MedicaidSDK
 // }
 
+
 let medicaidSDK = new MedicaidSDK();
 let s = `Performance rates on frequently reported health care quality measures in the CMS Medicaid/CHIP Child and Adult Core Sets, for FFY 2017 reporting.  Source: Mathematica analysis of MACPro and Form CMS-416 reports for the FFY 2017 reporting cycle. For more information, see the <a href="https://www.medicaid.gov/medicaid/quality-of-care/performance-measurement/child-core-set/index.html">Children's Health Care Quality Measures</a> and <a href="https://www.medicaid.gov/medicaid/quality-of-care/performance-measurement/adult-core-set/index.html">Adult Health Care Quality Measures</a> webpages.`
 medicaidSDK.getItemByDescription(s).then(r => console.log(r[0].title));
