50c5205042bdb62ab568944999974a029b6bfc79
KunaalAgarwal <aad5ha@virginia.edu>
2023-06-06 16:19:00 -0400

    added methods to sdk

--- metastore.js
+++ metastore.js
@@ -1,10 +1,19 @@
 // title: '2017 Child and Adult Health Care Quality Measures'
 // let s = `Performance rates on frequently reported health care quality measures in the CMS Medicaid/CHIP Child and Adult Core Sets, for FFY 2017 reporting.  Source: Mathematica analysis of MACPro and Form CMS-416 reports for the FFY 2017 reporting cycle. For more information, see the <a href="https://www.medicaid.gov/medicaid/quality-of-care/performance-measurement/child-core-set/index.html">Children's Health Care Quality Measures</a> and <a href="https://www.medicaid.gov/medicaid/quality-of-care/performance-measurement/adult-core-set/index.html">Adult Health Care Quality Measures</a> webpages.`
 // keyword 'performance rates'
-// identifier: 'c1028fdf-2e43-5d5e-990b-51ed03428625'
+// identifier: 'c1028fdf-2e43-5d5e-990b-51ed03428625' and '53426d8c-82b5-5dec-b44b-0f935b4603e5'
 import {fetchItems} from './sdk.js';
 
 //endpoint returns all databases and some metadata
+
+async function getAllItems(){
+    try{
+        return await fetchItems("metastore/schemas/dataset/items");
+    } catch (Error){
+        console.log("The request could not be fulfilled.");
+    }
+}
+
 async function getItemByTitleName(databaseTitle) {
     try{
         const items =  await fetchItems("metastore/schemas/dataset/items");
@@ -51,9 +60,14 @@ async function getItemByIdentifier(dbIdentifier){
         console.log("The request could not be fulfilled.");
     }
 }
+async function getItemTitle(identifier){
+    return (await getItemByIdentifier(identifier)).title;
+}
 
-
-
+async function getItemDownloadLink(identifier){
+    let distributionJson = (await getItemByIdentifier(identifier)).distribution
+    return distributionJson[0].downloadURL;
+}
 
 export {
     getItemByTitleName,
@@ -63,8 +77,14 @@ export {
     filterItemsByIdentifier
 }
 
-getItemByIdentifier('c1028fdf-2e43-5d5e-990b-51ed03428625').then(r => console.log(r));
-getItemByIdentifier('c1028fdf-2e43-5d5e-990b-51ed03428625').then(r => console.log(r));
+getItemTitle('c1028fdf-2e43-5d5e-990b-51ed03428625').then(r => console.log(r));
+getItemDownloadLink('c1028fdf-2e43-5d5e-990b-51ed03428625').then(r => console.log(r));
 
-filterItemsByIdentifier('c1028fdf-2e43-5d5e-990b-51ed03428625').then(r => console.log(r));
-getItemByTitleName('2017 Child and Adult Health Care Quality Measures').then(r => console.log(r));
+
+// getAllItems().then(r => console.log(r));
+
+// getItemByIdentifier('c1028fdf-2e43-5d5e-990b-51ed03428625').then(r => console.log(r));
+// getItemByIdentifier('c1028fdf-2e43-5d5e-990b-51ed03428625').then(r => console.log(r));
+//
+// filterItemsByIdentifier('c1028fdf-2e43-5d5e-990b-51ed03428625').then(r => console.log(r));
+// getItemByTitleName('2017 Child and Adult Health Care Quality Measures').then(r => console.log(r));
