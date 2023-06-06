807185725951aa1e2daef5341f441e0fccd91bd0
KunaalAgarwal <aad5ha@virginia.edu>
2023-06-06 11:32:30 -0400

    caching error fixed while using non OOP approach

--- sdk.js
+++ sdk.js
@@ -1,19 +1,24 @@
+// title: '2017 Child and Adult Health Care Quality Measures'
 // let s = `Performance rates on frequently reported health care quality measures in the CMS Medicaid/CHIP Child and Adult Core Sets, for FFY 2017 reporting.  Source: Mathematica analysis of MACPro and Form CMS-416 reports for the FFY 2017 reporting cycle. For more information, see the <a href="https://www.medicaid.gov/medicaid/quality-of-care/performance-measurement/child-core-set/index.html">Children's Health Care Quality Measures</a> and <a href="https://www.medicaid.gov/medicaid/quality-of-care/performance-measurement/adult-core-set/index.html">Adult Health Care Quality Measures</a> webpages.`
-// medicaidSDK.getItemByDescription(s).then(r => console.log(r[0].title));
-// medicaidSDK.getItemByIdentifier('c1028fdf-2e43-5d5e-990b-51ed03428625').then(r => console.log(r[0].title));
-// medicaidSDK.getItemByTitleName('2017 Child and Adult Health Care Quality Measures').then(r => console.log(r[0].title));
-// medicaidSDK.getItemByKeyword('performance rates').then(r => console.log(r[0].title));
+// keyword 'performance rates'
+// identifier: 'c1028fdf-2e43-5d5e-990b-51ed03428625'
 
 const loadedAt = `Medicaid SDK loaded at \n${Date()}`;
-let items = [] // caching
+let itemsPromise = null; // caching
 
-async function fetchItems(){
-    if(items.length>0){
-        return items // if cached no need to retrieve again
-    }else{
-        items=await (await fetch('https://data.medicaid.gov/api/1/metastore/schemas/dataset/items')).json()
+async function fetchItems() {
+    if (itemsPromise !== null) {
+        return itemsPromise;
+    } else {
+        itemsPromise = fetch('https://data.medicaid.gov/api/1/metastore/schemas/dataset/items')
+            .then((response) => response.json())
+            .catch((error) => {
+                itemsPromise = null; // Reset the promise if an error occurs
+                throw error;
+            });
+
         console.log(loadedAt);
-        return items
+        return itemsPromise;
     }
 }
 
@@ -38,11 +43,13 @@ async function getItemByIdentifier(dbIdentifier) {
 }
 
 export {
-    items,
     fetchItems,
     getItemByTitleName,
     getItemByKeyword,
     getItemByDescription,
     getItemByIdentifier
 }
+
+// getItemByIdentifier('c1028fdf-2e43-5d5e-990b-51ed03428625').then(r => console.log(r));
+// getItemByIdentifier('c1028fdf-2e43-5d5e-990b-51ed03428625').then(r => console.log(r));
 
