e91b7487f6e267dd422dff4c8fb6fef0ae872d8d
KunaalAgarwal <aad5ha@virginia.edu>
2023-06-13 19:09:55 -0400

    changed caching mechanism to use local storage

--- sdk/httpMethods.js
+++ sdk/httpMethods.js
@@ -1,31 +1,58 @@
 const loadedAt = `Medicaid SDK loaded at \n${Date()}`;
 const baseUrl = 'https://data.medicaid.gov/api/1/';
 let cache = {};
+// async function getItems(endpoint, downloadFlag = false) {
+//     if (cache[endpoint] !== undefined) {
+//         return cache[endpoint];
+//     } else {
+//         cache[endpoint] = fetch(baseUrl + endpoint)
+//             .then((response) => {
+//                 if (!response.ok) {
+//                     throw new Error(`API response was invalid`);
+//                 }
+//                 if (downloadFlag){
+//                     return response.blob();
+//                 }
+//                 return response.json();
+//             })
+//             .then((data) => {
+//                 cache[endpoint] = data; // Cache the response
+//                 return data;
+//             })
+//             .catch((error) => {
+//                 delete cache[endpoint]; // Remove the entry from cache in case of error
+//                 throw error;
+//             });
+//         return cache[endpoint];
+//     }
+// }
 async function getItems(endpoint, downloadFlag = false) {
-    if (cache[endpoint] !== undefined) {
-        return cache[endpoint];
+    const cacheKey = `cache_${endpoint}`;
+    // Check if the data exists in localStorage
+    const cachedData = localStorage.getItem(cacheKey);
+    if (cachedData !== null) {
+        // Return the deserialized data
+        return JSON.parse(cachedData);
     } else {
-        cache[endpoint] = fetch(baseUrl + endpoint)
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error(`API response was invalid`);
-                }
-                if (downloadFlag){
-                    return response.blob();
-                }
-                return response.json();
-            })
-            .then((data) => {
-                cache[endpoint] = data; // Cache the response
-                return data;
-            })
-            .catch((error) => {
-                delete cache[endpoint]; // Remove the entry from cache in case of error
-                throw error;
-            });
-        return cache[endpoint];
+        const response = await fetch(baseUrl + endpoint);
+        if (!response.ok) {
+            throw new Error(`API response was invalid`);
+        }
+
+        let responseData;
+        if (downloadFlag) {
+            responseData = await response.blob();
+        } else {
+            responseData = await response.json();
+        }
+
+        // Cache the serialized data in localStorage
+        localStorage.setItem(cacheKey, JSON.stringify(responseData));
+
+        return responseData;
     }
 }
+
 
 async function postItem(endpoint, payload, headerContent) {
     const options = {
@@ -74,3 +101,6 @@ export{
     postItem,
     postDownloadableItem
 }
+
+let a = 1;
+
