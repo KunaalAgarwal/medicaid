b57b1740054021c2279288c1d0754de3d2519e16
KunaalAgarwal <aad5ha@virginia.edu>
2023-06-15 12:05:06 -0400

    added in the correct post method

--- sdk/httpMethods.js
+++ sdk/httpMethods.js
@@ -23,6 +23,7 @@ async function getItems(endpoint, downloadFlag = false) {
     }
 }
 
+
 async function postItem(endpoint, payload, headerContent, downloadFlag = false) {
     const options = {
         method: 'POST',
@@ -30,22 +31,23 @@ async function postItem(endpoint, payload, headerContent, downloadFlag = false) 
         body: JSON.stringify(payload)
     };
     try {
+        const cachedData = await localforage.getItem(endpoint)
+        if (cachedData !== null){
+            return cachedData
+        }
         const response = await fetch(baseUrl + endpoint, options);
         if (response.ok){
             let responseData;
             if (downloadFlag){
                 responseData = await response.blob();
-                console.log(responseData)
             } else {
                 responseData = await response.json();
             }
+            await localforage.setItem(endpoint, responseData);
             return responseData
         }
-        else {
-            console.log("Invalid json or identifier");
-        }
     } catch (error) {
-        console.error('Error:', error.message);
+        console.log("An error occurred in the API post");
     }
 }
 
