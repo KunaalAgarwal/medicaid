0507e87caaa6bbfbdb771402edafc96144d16465
KunaalAgarwal <aad5ha@virginia.edu>
2023-07-24 15:13:57 -0400

    basing nadac plotting on ndc code, going to implement fda api to convert ndc codes to drug names

--- sdk/plot/nadac.js
+++ sdk/plot/nadac.js
@@ -6,11 +6,13 @@ const nadacDatasets = (await getDatasetByKeyword("nadac")).filter(r => r.title.i
 const nadacDistributions = await Promise.all(nadacDatasets.map(r => {return convertDatasetToDistributionId(r.identifier)}))
 
 async function getNadacMeds(){
+    //waiting on fda api
     const medlist = await getAllNdcs();
     return new Set(medlist.values());
 }
 
 async function getNdcFromMed(med){
+    //waiting on fda api
     const sql = `[SELECT ndc_description,ndc FROM f4ab6cb6-e09c-52ce-97a2-fe276dbff5ff][WHERE ndc_description = "${med}"][LIMIT 1]`;
     const response = await getDatastoreQuerySql(sql);
     return response[0]["ndc"]
@@ -22,14 +24,14 @@ async function getMedNames(medicine){
     return medList.filter(med => med.split(' ')[0] === `${baseMedName.toUpperCase()}`)
 }
 
-async function getMedData(meds, filter = "ndc_description", dataVariables = ["as_of_date", "nadac_per_unit"]){
+async function getMedData(meds, filter = "ndc", dataVariables = ["as_of_date", "nadac_per_unit"]){
     const rawData = await getAllData(meds, filter, nadacDistributions, dataVariables);
     return rawData.flat()
 }
 
 async function getMedDataPlot(meds, axis = {xAxis: "as_of_date", yAxis: "nadac_per_unit"}){
     const medList = Array.isArray(meds) ? meds : [meds];
-    const medData = await getMedData(medList, "ndc_description", Object.values(axis));
+    const medData = await getMedData(medList, "ndc", Object.values(axis));
     const result = medData.reduce(
         (result, obj) => {
             result.x.push(obj[axis.xAxis])
@@ -53,6 +55,7 @@ async function plotNadacMed(meds, layout, div, axis) {
 }
 
 async function getSimilarMeds(medList) {
+    //waiting on fda api
     let allSimMeds = [];
     for (let i of medList) {
         let meds = await getMedNames(i);
@@ -74,6 +77,7 @@ async function getSimilarMeds(medList) {
 }
 
 function parseSelectedMeds(medList) {
+    //waiting on fda api
     return Object.values(medList.reduce((result, obj) => {
         const {generalDrug, specificName} = obj;
         if (generalDrug in result) {
@@ -86,17 +90,17 @@ function parseSelectedMeds(medList) {
 }
 
 async function getAllNdcs() {
-    const ndcMap = new Map();
+    let startTime = Date.now();
+    const ndcs = new Map();
     for (let i = 0; i < nadacDistributions.length; i += 4){
         if (i > nadacDistributions.length){
             break;
         }
-        const response = await getDatastoreQuerySql(`[SELECT ndc_description,ndc FROM ${nadacDistributions[i]}]`)
-        response.forEach(med => {
-            ndcMap.set(med, med["ndc_description"]);
-        })
+        const response = await getDatastoreQuerySql(`[SELECT ndc FROM ${nadacDistributions[i]}]`);
+        response.forEach(ndc => {ndcs.set(ndc["ndc"], 1);})
     }
-    return ndcMap;
+    console.log(`Grabbed Data: ${Date.now() - startTime}`)
+    return [...ndcs.keys()];
 }
 
 export {
