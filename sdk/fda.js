7eba2c730bd65233018b3c00b969feb521b7f67b
KunaalAgarwal <aad5ha@virginia.edu>
2023-08-15 23:24:06 -0400

    adjustment to fda.js

--- sdk/fda.js
+++ sdk/fda.js
@@ -3,7 +3,9 @@ async function getDrugContext(rxcui){
     try{
         const response = await getItems(`search=openfda.rxcui.exact:"${rxcui}"`, false, `https://api.fda.gov/drug/ndc.json?`);
         if (response === undefined) throw new Error("Drug context isn't available for this drug");
-        return response["results"];
+        const result = Array.isArray(response["results"]) ? response["results"] : [response["results"]];
+        if (result.length === 1) return result[0];
+        return result;
     } catch (error) {
         console.log("Drug context isn't available for this drug")
     }
